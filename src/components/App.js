3f7ea85442cd0591149e06d2a5991dc57f8722e5
Dana <dana.lachman@gmail.com>
2017-01-23 16:59:04 -0600

    rendering full array of css colors sorted by hue

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import tinycolor from 'tinycolor2';
+import colors from '../data/colors.js';
 
 // style objects
 const divStyle = {
@@ -14,46 +15,43 @@ const colorHex = color.toHexString();
 
 console.log(color, format, color._r, colorHex);
 
+// console.log(MyColors);
 
-
-// class Colors extends Component {
-//   render() {
-//     return (
-//       <svg width="50" height="50">
-//         <circle cx="25" cy="25" r="25" fill="gold" />
-//       </svg>
-//     )
-//   }
-// }
-
+// Colors component to map over list of colors and make me some circles
 const Colors = props => {
   const colors = props.colors;
   const colorCircles = colors.map((color) => {
     return (
-      <svg key={color} width="50" height="50">
-      <circle cx="25" cy="25" r="25" fill="gold" />
+      <svg key={color} width="100" height="100">
+      <circle cx="50" cy="50" r="40" fill={color} />
       </svg>
     );
   }
 
   );
   return (
-    <div>
+    <div className="named-color-set">
       {colorCircles}
     </div>
   );
 }
 
-
-let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-
-
+// this is my list of colors for now.
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    e.preventDefault();
+    console.log('clickit!')
+  }
+
   render() {
     return (
       <div className="App" style={divStyle}>
           <h2>Goodbye Blue Monday</h2>
-          <Colors colors={colors} />
+          <Colors colors={colors} onClick={this.handleClick}/>
       </div>
     );
   }
