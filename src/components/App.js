726011b8497cd343be28690991ae9618d83c15b8
Dana <dana.lachman@gmail.com>
2017-01-23 21:46:38 -0600

    click handler functional and I'm grabbing the correct data.

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,10 @@ const divStyle = {
   color: "#5c5c5c",
 };
 
+const logoDivStyle = {
+  border: "2px solid magenta"
+}
+
 // tinycolor2 variables
 const color = tinycolor("gold");
 const format = color.getFormat();
@@ -20,10 +24,13 @@ console.log(color, format, color._r, colorHex);
 // Colors component to map over list of colors and make me some circles
 const Colors = props => {
   const colors = props.colors;
+
   const colorCircles = colors.map((color) => {
+    // let myColor = tinycolor(color).toHexString();
+    // console.log(myColor);
     return (
       <svg key={color} width="100" height="100">
-      <circle cx="50" cy="50" r="40" fill={color} />
+        <circle cx="50" cy="50" r="40" fill={color} onClick={props.onClick} />
       </svg>
     );
   }
@@ -44,14 +51,21 @@ class App extends Component {
   }
   handleClick(e) {
     e.preventDefault();
-    console.log('clickit!')
+    const colorName = e.target.getAttribute('fill');
+    const colorHex = tinycolor(colorName).toHexString();
+    const colorRgb = tinycolor(colorName).toRgbString();
+    const colorHsl = tinycolor(colorName).toHslString();
+    console.log('clickit!', colorName, colorHex, colorRgb, colorHsl);
   }
 
   render() {
     return (
       <div className="App" style={divStyle}>
-          <h2>Goodbye Blue Monday</h2>
-          <Colors colors={colors} onClick={this.handleClick}/>
+        <h2>Goodbye Blue Monday</h2>
+        <div style={logoDivStyle}>
+          hello. logo time.
+        </div>
+        <Colors colors={colors} onClick={this.handleClick}/>
       </div>
     );
   }
